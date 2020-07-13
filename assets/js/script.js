609f3c51da21f38dc69b27a83fb413fdf6bd1425
Kelechi-Eluagu <kellyeluagu@gmail.com>
2020-07-13 18:17:06 +0100

    added card to dashboard

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,14 @@ $("#menu-toggle").click(function(e) {
     e.preventDefault();
     $("#wrapper").toggleClass("toggled");
 });
+$('.dashboar-count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 5000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
